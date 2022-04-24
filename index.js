206fc14a6593b43c2b013cd1679a5035eab9a526
Andres Rivero <andres19rivero@gmail.com>
2022-04-24 13:25:09 -0400

    application to the internet

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
+
+app.use(cors())
 
 let notes = [
 	{
@@ -86,7 +89,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
