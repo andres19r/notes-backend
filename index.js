71db7488f591f1073197279d6ada301dd16f02a9
Andres Rivero <andres19rivero@gmail.com>
2022-04-28 23:50:35 -0400

    feat: update and delete operations

--- index.js
+++ index.js
@@ -4,28 +4,6 @@ const cors = require("cors");
 const app = express();
 const Note = require("./models/note");
 
-app.use(cors());
-
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -35,9 +13,10 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(express.static("build"));
 app.use(express.json());
 app.use(requestLogger);
+app.use(cors());
+app.use(express.static("build"));
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
@@ -61,10 +40,12 @@ app.get("/api/notes/:id", (request, response, next) => {
     .catch(error => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 });
 
 const generateId = () => {
@@ -86,6 +67,21 @@ app.post("/api/notes", (request, response) => {
     response.json(savedNote);
   });
 });
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
 const unknownEndpoint = (req, res) => {
   res.status(404).send({ error: "unknown endpoint" });
