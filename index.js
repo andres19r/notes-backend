370b49ea85c527d4a69ef7a549337502949988cf
Andres Rivero <andres19rivero@gmail.com>
2022-04-28 23:30:44 -0400

    feat: error handling

--- index.js
+++ index.js
@@ -1,95 +1,111 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const Note = require('./models/note')
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const Note = require("./models/note");
 
-app.use(cors())
+app.use(cors());
 
 let notes = [
-	{
-		id: 1,
-		content: "HTML is easy",
-		date: "2022-05-30T17:30:31.098Z",
-		important: true
-	},
-	{
-		id: 2,
-		content: "Browser can execute only Javascript",
-		date: "2022-05-30T18:39:34.091Z",
-		important: false
-	},
-	{
-		id: 3,
-		content: "GET and POST are the most important methods of HTTP protocol",
-		date: "2022-05-30T19:20:14.298Z",
-		important: true
-	}
-]
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2022-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2022-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2022-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
 
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
 
-app.use(express.json())
+app.use(express.static("build"));
+app.use(express.json());
+app.use(requestLogger);
 
-app.use(express.static('build'))
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!</h1>");
+});
 
-app.use(requestLogger)
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
-app.get('/', (request, response) => {
-	response.send('<h1>Hello World!</h1>')
-})
+app.get("/api/notes/:id", (request, response, next) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error));
+});
 
-app.get('/api/notes', (request, response) => {
-	Note.find({}).then(notes => {
-		response.json(notes)
-	})
-})
-
-app.get('/api/notes/:id', (request, response) => {
-	Note.findById(request.params.id).then(note => {
-		response.json(note)
-	})
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-	const id = Number(request.params.id)
-	notes = notes.filter(note => note.id !== id)
-	response.status(204).end()
-})
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  notes = notes.filter((note) => note.id !== id);
+  response.status(204).end();
+});
 
 const generateId = () => {
-	const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0
-	return maxId + 1
-}
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-app.post('/api/notes', (request, response) => {
-	const body = request.body
-	if (body.content === undefined) {
-		return response.status(400).json({ error: 'content missing' })
-	}
-	const note = new Note({
-		content: body.content,
-		important: body.important || false,
-		date: new Date(),
-	})
-	note.save().then(savedNote => {
-		response.json(savedNote)
-	})
-})
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+  if (body.content === undefined) {
+    return response.status(400).json({ error: "content missing" });
+  }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  });
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
+});
 
 const unknownEndpoint = (req, res) => {
-	res.status(404).send({ error: 'unknown endpoint' })
+  res.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
 }
 
-app.use(unknownEndpoint)
+app.use(errorHandler)
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
