95844d8dabc0bd4b1e028b1fd3f31cba187ab479
Andres Rivero <andres19rivero@gmail.com>
2022-05-02 23:41:42 -0400

    fix: typo

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.put('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndUpdate(
     request.params.id,
     { content, important },
-    { new: trues, runValidators: true, context: 'query' }
+    { new: true, runValidators: true, context: 'query' }
   )
     .then(updatedNote => {
       response.json(updatedNote)
